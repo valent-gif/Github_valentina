af4135892ea6e71124a5839a65633e247b8803ae
valent-gif <valentinarojasmoratto@gmail.com>
2026-09-18 07:38:42 -0500

    feat: :zap: se hace una lectura respectiva en la parte de las escrituras
    en lecturas imprime un mensaje y en escrituras imprime otr mensaje

--- diccionarios/diccionario.py
+++ diccionarios/diccionario.py
@@ -1,3 +1,5 @@
 #leer un diccionario y calcular el promedio
 #DEsarrollador1 se va a encargar de diccionario
 diccionario = {"nombre": "desarrollador1"}
+print(diccionario)
+print("El diccionario tiene", len(diccionario), "elementos")
